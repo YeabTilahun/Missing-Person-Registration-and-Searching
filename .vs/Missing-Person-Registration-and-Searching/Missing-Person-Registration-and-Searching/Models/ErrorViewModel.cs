9845cdd6e3c7e8d8a058a966d7c6ad5d111c3dfc
Yeabsira <yeabsiratilahun78@gmail.com>
2023-05-22 08:17:46 +0300

    Removed failed project and added a new one

--- .vs/Missing-Person-Registration-and-Searching/Missing-Person-Registration-and-Searching/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Missing_Person_Registration_and_Searching.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Missing-Person/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Missing_Person.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
