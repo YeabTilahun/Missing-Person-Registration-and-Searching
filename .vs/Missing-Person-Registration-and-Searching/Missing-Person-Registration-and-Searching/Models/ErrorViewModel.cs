ec4a467138410aa70674a465e36f6aca4543a1e4
Yeabsira <yeabsiratilahun78@gmail.com>
2023-05-22 08:03:51 +0300

    Initial commit

---
+++ .vs/Missing-Person-Registration-and-Searching/Missing-Person-Registration-and-Searching/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Missing_Person_Registration_and_Searching.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
